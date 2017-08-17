115a3f7e4e987e661ef9e7217ba7cf02a5120cb9
Sunil G <sunilg@apache.org>
2017-08-17 21:14:51 +0530

    YARN-7030. [YARN-3926] Performance optimizations in Resource and ResourceUtils class. Contributed by Wangda Tan.
    (cherry picked from commit a6989af1d91d8b6795862c39d154823f8488c8de)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/util/resource/ResourceUtils.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/util/resource/ResourceUtils.java
@@ -71,6 +71,7 @@ public class ResourceUtils {
   private static volatile ResourceInformation[] resourceTypesArray;
   private static volatile boolean initializedNodeResources = false;
   private static volatile Map<String, ResourceInformation> readOnlyNodeResources;
+  private static volatile int numKnownResourceTypes = -1;
 
   static final Log LOG = LogFactory.getLog(ResourceUtils.class);
 
@@ -226,13 +227,23 @@ public class ResourceUtils {
    * @return resourceNamesArray
    */
   public static String[] getResourceNamesArray() {
-    getResourceTypes(null, YarnConfiguration.RESOURCE_TYPES_CONFIGURATION_FILE);
+    initializeResourceTypesIfNeeded(null,
+        YarnConfiguration.RESOURCE_TYPES_CONFIGURATION_FILE);
     return resourceNamesArray;
   }
 
   public static ResourceInformation[] getResourceTypesArray() {
-    getResourceTypes(null, YarnConfiguration.RESOURCE_TYPES_CONFIGURATION_FILE);
+    initializeResourceTypesIfNeeded(null,
+        YarnConfiguration.RESOURCE_TYPES_CONFIGURATION_FILE);
     return resourceTypesArray;
+  }
+
+  public static int getNumberOfKnownResourceTypes() {
+    if (numKnownResourceTypes < 0) {
+      initializeResourceTypesIfNeeded(null,
+          YarnConfiguration.RESOURCE_TYPES_CONFIGURATION_FILE);
+    }
+    return numKnownResourceTypes;
   }
 
   private static Map<String, ResourceInformation> getResourceTypes(
@@ -241,8 +252,8 @@ public class ResourceUtils {
         YarnConfiguration.RESOURCE_TYPES_CONFIGURATION_FILE);
   }
 
-  private static Map<String, ResourceInformation> getResourceTypes(
-      Configuration conf, String resourceFile) {
+  private static void initializeResourceTypesIfNeeded(Configuration conf,
+      String resourceFile) {
     if (!initializedResources) {
       synchronized (ResourceUtils.class) {
         if (!initializedResources) {
@@ -264,6 +275,12 @@ public class ResourceUtils {
         }
       }
     }
+    numKnownResourceTypes = resourceTypes.size();
+  }
+
+  private static Map<String, ResourceInformation> getResourceTypes(
+      Configuration conf, String resourceFile) {
+    initializeResourceTypesIfNeeded(conf, resourceFile);
     return resourceTypes;
   }
 
